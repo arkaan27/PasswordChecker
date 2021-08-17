d2611e3a405e3cfb0738d1625881b0e4677c6fad
arkaan27 <arkaankanunga@gmail.com>
2021-08-17 12:17:53 +0100

    First commit

---
+++ Statement1.java
@@ -0,0 +1,9 @@
+public class Statement1 {
+    public static void main(String[] args){
+        System.out.println(2 + 3);
+        int x= 5;
+        int y = 5;
+        int z= x + y;
+        System.out.println(z);
+    }
+}
