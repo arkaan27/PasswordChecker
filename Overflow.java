87b3dfea305e7cbd96d352f507205e4676f78277
arkaan27 <arkaankanunga@gmail.com>
2021-08-17 14:08:30 +0100

    Overflow

---
+++ Overflow.java
@@ -0,0 +1,16 @@
+public class Overflow {
+    public static void main(String[] args){
+        byte x= 14;
+        char myChar= 'Y';
+        double myDouble= 2.5d;
+        int m= 307;
+        byte n= -1;
+        long l= 438000000L;
+        boolean k= false;
+        System.out.println(x);
+        System.out.println(myChar + "Hello");
+        System.out.println(myDouble);
+        System.out.println(m +  " " + n + " " + l);
+        System.out.println(k);
+    }
+}
